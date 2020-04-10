2b0be15df2d6e66abe291b34e2f711a14740fdbe
Live session user <mint@etudiant.oclock.io>
2020-04-10 16:17:16 +0200

    Install Symfony and React

---
+++ sym/src/Controller/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+    * @Route("/", name="api")
+    */
+   public function browse()
+   {
+       return $this->json('coucou');
+   }
+}
