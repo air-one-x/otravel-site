9bc2eef0fd8a9f01d22378e9d3225d248001543e
Live session user <mint@etudiant.oclock.io>
2020-04-10 16:18:50 +0200

    Install Symfony and React

---
+++ React-modele-master/src/components/App/index.js
@@ -0,0 +1,26 @@
+// == Import npm
+import React from 'react';
+import axios from 'axios';
+
+// == Import
+import Counter from 'src/containers/Counter';
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+// == Composant
+const App = () => {
+  axios({
+    url:'http://127.0.0.1:8002/',
+    method:'get'
+  }).then((response) => console.log(response.data)).catch((error) => console.log(error));
+    return (
+      <div className="app">
+        <img src={reactLogo} alt="react logo" />
+        <h1>Composant : App</h1>
+        <Counter />
+      </div>
+    )
+};
+
+// == Export
+export default App;
