4e85341941e286aa41a8f2e8865220ea1d620184
Alexandre TECHER <alexandre.oclock@gmail.com>
2020-04-12 17:26:04 +0200

    BDD creation and API start

---
+++ sym/src/Entity/CommentaryPicture.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentaryPictureRepository")
+ */
+class CommentaryPicture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups("commentary_picture")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups("commentary_picture")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups("commentary_picture")
+     */
+    private $uploaded_at;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Commentary", inversedBy="CommentaryPicture")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups("commentary_picture")
+     */
+    private $commentary;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUploadedAt(): ?\DateTimeInterface
+    {
+        return $this->uploaded_at;
+    }
+
+    public function setUploadedAt(\DateTimeInterface $uploaded_at): self
+    {
+        $this->uploaded_at = $uploaded_at;
+
+        return $this;
+    }
+
+    public function getCommentary(): ?Commentary
+    {
+        return $this->commentary;
+    }
+
+    public function setCommentary(?Commentary $commentary): self
+    {
+        $this->commentary = $commentary;
+
+        return $this;
+    }
+}
---
+++ sym/src/Entity/User.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups("user")     
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups("user")
+     */
+    private $avatar;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups("user")
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups("user")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups("user")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @Groups("user")
+     */
+    private $updated_at;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Places", mappedBy="user")
+     *@Groups("user")
+     */
+    private $Places;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Commentary", mappedBy="user", orphanRemoval=true)
+     * @Groups("user")
+     */
+    private $Commentary;
+
+    public function __construct()
+    {
+        $this->Places = new ArrayCollection();
+        $this->Commentary = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(?string $avatar): self
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Places[]
+     */
+    public function getPlaces(): Collection
+    {
+        return $this->Places;
+    }
+
+    public function addPlace(Places $place): self
+    {
+        if (!$this->Places->contains($place)) {
+            $this->Places[] = $place;
+            $place->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removePlace(Places $place): self
+    {
+        if ($this->Places->contains($place)) {
+            $this->Places->removeElement($place);
+            // set the owning side to null (unless already changed)
+            if ($place->getUser() === $this) {
+                $place->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Commentary[]
+     */
+    public function getCommentary(): Collection
+    {
+        return $this->Commentary;
+    }
+
+    public function addCommentary(Commentary $commentary): self
+    {
+        if (!$this->Commentary->contains($commentary)) {
+            $this->Commentary[] = $commentary;
+            $commentary->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommentary(Commentary $commentary): self
+    {
+        if ($this->Commentary->contains($commentary)) {
+            $this->Commentary->removeElement($commentary);
+            // set the owning side to null (unless already changed)
+            if ($commentary->getUser() === $this) {
+                $commentary->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+}
