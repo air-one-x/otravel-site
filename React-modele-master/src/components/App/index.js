8523a00376a9dcbc478300fd9fdb04711f856353
gaetanLareur <g.lareur@gmail.com>
2020-04-13 18:44:29 +0200

    new navbar

--- React-modele-master/src/components/App/index.js
+++ React-modele-master/src/components/App/index.js
@@ -1,16 +1,21 @@
 // == Import npm
 import React from 'react';
 import axios from 'axios';
+import './style.css'
 
 // == Import
 import NavBar from '../Navbar/Navbar';
+import MapContainer from '../MapContainer';
 import NavBarBottom from '../NavBarBottom';
+
+
 // == Composant
 const App = () => (
 
   <div className="app">
     <NavBar />
-    <NavBarBottom />
+    <MapContainer />
+    <NavBarBottom  className="nav--bar--bottom"/>    
   </div>
 );
 
--- React-modele-master/src/components/Navbar/Navbar.js
+++ React-modele-master/src/components/Navbar/Navbar.js
@@ -1,63 +1,20 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import {IconButton,InputBase } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import useStyles from './style';
-import ItemsMenu from './itemsMenu/ItemsMenu'
+import React from 'react';
+import './style.css';
 
-
-function NavBar() {
-    
-  const classes = useStyles();
-
-  //openmenu
-  const [openMenu, setOpenMenu] = React.useState(null);
-
-  //click menu burger
-  const handleClick = (event) => {
-    setOpenMenu(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setOpenMenu(null);
-  };
+const NavBar = () => {
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-      
-          <Typography className={classes.title} variant="h6" noWrap>
-            o'Travel
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-          <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleClick}
-        >
-          <MenuIcon />
-        </IconButton>
-        <ItemsMenu openMenu={openMenu} closeMenu={handleClose} />
-        </Toolbar>
-      </AppBar>
+    <div class="btn-group">
+      <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Action
+      </button>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Separated link</a>
+      </div>
     </div>
   );
 }
