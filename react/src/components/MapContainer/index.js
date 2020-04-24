df441b79476e2ff5a61f9d46f259fa80f3f14525
gaetanLareur <g.lareur@gmail.com>
2020-04-24 19:08:53 +0200

    add place on click

--- react/src/components/MapContainer/index.js
+++ react/src/components/MapContainer/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Map, Marker, Popup, TileLayer,
 } from 'react-leaflet';
@@ -7,7 +7,7 @@ import ReactLeafletSearch from "react-leaflet-search";
 import L from 'leaflet';
 import userLocationURL from './map-pin-solid.svg';
 import NavBar from '../../containers/navBarFake';
-
+import AddPlaceModal from '../../containers/AddPlace';
 
 const myIcon = L.icon({
   iconUrl: userLocationURL,
@@ -29,7 +29,12 @@ const MapContainer = (props) => {
     isFilterToilet,
     setViewport } = props;
 
-  const [activePlace, setActivePlace] = React.useState(null);
+  const [activePlace, setActivePlace] = useState(null);
+  const [latMarker, setlatMarker] = useState(0);
+  const [lngMarker, setlngMarker] = useState(0);
+  const [markerClick, setMarkerClick] = useState(false);
+  const [popupMarkerClick, setPopupMarkerClick] = useState(false);
+  const [activePopupMarkerClick, setActivePopupMarkerClick] = useState(null);
 
   const getLocation = () => {
     if (navigator.geolocation) {
@@ -43,6 +48,21 @@ const MapContainer = (props) => {
     userLocation(position.coords)
   }
 
+  const test = (event) => {
+    console.log('click sur la map', event)
+    setMarkerClick(true);
+    setlatMarker(event.latlng.lat),
+    setlngMarker(event.latlng.lng)
+  }
+
+  const test2 = (event) => {
+    console.log('itinaore', event)
+    setPopupMarkerClick(true)
+    setActivePopupMarkerClick(event.latlng)
+    
+  }
+
+
   useEffect(() => {
      getLocation()
 
@@ -54,7 +74,7 @@ const MapContainer = (props) => {
     <div>
       <NavBar />
       <div className="map" id="mapid">
-        <Map viewport={viewport} minZoom="3">
+        <Map viewport={viewport} minZoom="3" onClick={test} >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -71,6 +91,16 @@ const MapContainer = (props) => {
             providerOptions={{searchBounds: []}} // The BingMap and OpenStreetMap providers both accept bounding coordinates in [se,nw] format. Note that in the case of OpenStreetMap, this only weights the results and doesn't exclude things out of bounds.
             customProvider={undefined | {search: (searchString)=> {}}} // see examples to usage details until docs are ready
           />
+           
+          {
+            markerClick &&(
+            <Marker
+              position={[latMarker, lngMarker]}
+              icon={myIcon}
+              onClick={test2}
+            >
+            </Marker>)
+          }
           {isLocated &&(
             <Marker
               position={[lat, long]}
@@ -135,6 +165,22 @@ const MapContainer = (props) => {
             </div>
           </Popup>
           )}
+          {activePopupMarkerClick && isLogged&& (
+            <Popup
+              position={[
+                activePopupMarkerClick.lat,
+                activePopupMarkerClick.lng,
+              ]}
+              onClose={() => {
+                setActivePopupMarkerClick(null);
+              }}
+            >
+              <div>
+              <AddPlaceModal/>
+              </div>
+            </Popup>
+            )}
+
         </Map>
       </div>
     </div>
