f87cdb4aae0f3f40a20581257e7a18387ec3dd03
gaetanLareur <g.lareur@gmail.com>
2020-04-27 17:11:22 +0200

    fix bug

--- react/src/components/Account/InformationsUser/index.js
+++ react/src/components/Account/InformationsUser/index.js
@@ -7,35 +7,7 @@ import InputComponent from '../../Generique/inputComponent';
 
 const InformationsUser = ({username, email, date, contributions, inputChangeEmailInformation, inputChangePseudoInformation, inputChangePasswordInformation, newUserPassword, newUserEmail, newUserPseudo, insertNewInformation, updateMessage, userAvatar}) => {      
     
-    console.warn(inputChangeEmailInformation);
 
-    const newDate = new Date(date);
-    const day = newDate.getDay();
-    const day1 = day+1;
-    const month = newDate.getMonth();
-    const month1 = month+1;
-    const year = newDate.getFullYear();
-
-
-        useEffect(()=> {    
-            
-    let avatar = document.getElementById('user__picture');
-   let test = document.createElement("img");
-        test.src = img;
-        test.style.height= '50px';
-        test.style.width='50px';
-        test.style.borderRadius='50%';
-        test.style.margin='auto';
-        test.style.textAlign='center';
-        console.log(test);
-        avatar.append(test);
-
-        },[])
-     
-
-  
-    
-  
     return(
     <div className="informations--user">
         <header>
@@ -48,7 +20,6 @@ const InformationsUser = ({username, email, date, contributions, inputChangeEmai
                 <img src={`http://localhost:8001/${userAvatar}`} ></img>
            </div>
            <div className="user--statistic">
-               <div>Membres depuis le : <time>{`${day1}/${month1}/${year}`}</time></div>
                <div className="user--additions">Contribution: <span>{contributions != undefined ? contributions.length : "0"}</span> ajouts</div>
            </div>
            <div className="user--all--data">
